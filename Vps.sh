8b9590789040a8b928371da498da080b37a6dee6
YashasviStudent <89289289+YashasviStudent@users.noreply.github.com>
2021-10-07 13:13:21 +0530

    Create Vps.sh

---
+++ Vps.sh
@@ -0,0 +1,7 @@
+echo "Free Vps Ubuntu 24.04 LTS Google Cloud"
+echo "Starting Installation"
+docker run -p 32722:6901 accetto/ubuntu-vnc-xfce-g3
+echo "-----------------------------------------------------------------------------"
+echo "Installlation Completed"
+echo "To Get Into The Vps Change port to 32722 and Then Click Web Preview"
+echo "Keep Supporting Yashasvi Khandelwal & Yashasvi Gamerz!"
